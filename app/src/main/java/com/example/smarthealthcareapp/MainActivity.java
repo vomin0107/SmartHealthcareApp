3b3d23df32d5eef52be385f36a4c1e5adb223a13
vomin0107 <48756394+vomin0107@users.noreply.github.com>
2020-06-03 10:40:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/smarthealthcareapp/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.smarthealthcareapp;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentActivity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends FragmentActivity {
+    private WebView mWebView;
+
+    private String healthcareUrl = "https://smart-healthcare.elsiff.me/";
+
+    public ValueCallback<Uri> filePathCallbackNormal;
+    public ValueCallback<Uri[]> filePathCallbackLollipop;
+    public final static int FILECHOOSER_NORMAL_REQ_CODE = 2001;
+    public final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2002;
+    private Uri cameraImageUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        String[] PERMISSIONS = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+        };
+        for (String permission : PERMISSIONS){
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"운동/음식 인식을 위한 권한 승인이 필요합니다",Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        PERMISSIONS,
+                        0);
+                break;
+            }
+        }
+
+        // 웹뷰 셋팅
+        mWebView = (WebView) findViewById(R.id.mWebView); //xml 자바코드 연결
+        mWebView.getSettings().setJavaScriptEnabled(true);//자바스크립트 허용
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                Log.d("MainActivity", "5.0+");
+
+                // Callback 초기화 (중요!)
+                if (filePathCallbackLollipop != null) {
+                    filePathCallbackLollipop.onReceiveValue(null);
+                    filePathCallbackLollipop = null;
+                }
+                filePathCallbackLollipop = filePathCallback;
+
+                boolean isCapture = fileChooserParams.isCaptureEnabled();
+                runCamera(isCapture);
+                return true;
+            }
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(healthcareUrl);//웹뷰 실행
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(mWebView.canGoBack()){
+                mWebView.goBack();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode)
+        {
+            case FILECHOOSER_NORMAL_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackNormal == null) return;
+                    Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+                    filePathCallbackNormal.onReceiveValue(result);
+                    filePathCallbackNormal = null;
+                }
+                break;
+            case FILECHOOSER_LOLLIPOP_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackLollipop == null) return;
+                    if (data == null)
+                        data = new Intent();
+                    if (data.getData() == null)
+                        data.setData(cameraImageUri);
+
+                    filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                    filePathCallbackLollipop = null;
+                }
+                else
+                {
+                    if (filePathCallbackLollipop != null)
+                    {
+                        filePathCallbackLollipop.onReceiveValue(null);
+                        filePathCallbackLollipop = null;
+                    }
+
+                    if (filePathCallbackNormal != null)
+                    {
+                        filePathCallbackNormal.onReceiveValue(null);
+                        filePathCallbackNormal = null;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void runCamera(boolean _isCapture)
+    {
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File path = getFilesDir();
+        File file = new File(path, "Food.png");
+        // File 객체의 URI 를 얻는다.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            String strpa = getApplicationContext().getPackageName();
+            cameraImageUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".fileprovider", file);
+        }
+        else
+        {
+            cameraImageUri = Uri.fromFile(file);
+        }
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+
+        if (!_isCapture)
+        { // 선택팝업 카메라, 갤러리 둘다 띄우고 싶을 때..
+            Intent pickIntent = new Intent(Intent.ACTION_PICK);
+            pickIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            pickIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            String pickTitle = "사진 가져올 방법을 선택하세요.";
+            Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+
+            // 카메라 intent 포함시키기..
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{intentCamera});
+            startActivityForResult(chooserIntent, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
+        else
+        {// 바로 카메라 실행..
+            startActivityForResult(intentCamera, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
+    }
+}
